1e1c6cfca515b23986f418c064289ffda65c9912
DevArthurBarboza <arthur.barboza0016@edu.unifil.br>
2021-10-13 16:47:04 -0300

    Importing and applying JS on HTML

---
+++ index.js
@@ -0,0 +1,15 @@
+function teste() {
+  document.getElementById("demo").style.display = "none";
+}
+
+function escreva() {
+  document.getElementById("demo2").innerHTML = "Este é o parágrafo 2";
+}
+
+function output() {
+  window.alert("Testando 'alert'");
+}
+
+function consoleLog() {
+  console.log("Testando console.log");
+}
